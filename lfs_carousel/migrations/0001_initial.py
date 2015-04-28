a6066418b13dd2be355e5dab0a98e7ddd5a8e497
Pigletto <pigletto@gmail.com>
2015-04-28 22:52:12 +0200

    updated django migrations

--- lfs_carousel/migrations/0001_initial.py
+++ lfs_carousel/migrations/0001_initial.py
@@ -18,7 +18,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('content_id', models.PositiveIntegerField(null=True, verbose_name='Content id', blank=True)),
                 ('title', models.CharField(max_length=100, verbose_name='Title', blank=True)),
-                ('image', lfs.core.fields.thumbs.ImageWithThumbsField(upload_to=b'images', null=True, verbose_name='Image', blank=True)),
+                ('image', lfs.core.fields.thumbs.ImageWithThumbsField(upload_to=b'images', null=True, verbose_name='Image', sizes=((60, 60), (90, 90), (100, 100), (200, 200), (300, 300), (400, 400), (530, 345), (1024, 768)), blank=True)),
                 ('link', models.URLField(default=b'', null=True, verbose_name='URL', blank=True)),
                 ('text', models.TextField(default=b'', verbose_name='Text', blank=True)),
                 ('position', models.PositiveSmallIntegerField(default=999, verbose_name='Position')),
